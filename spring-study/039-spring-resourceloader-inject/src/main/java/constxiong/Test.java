9a3e5be0efbfa4ded3dab4e37d45342256acb344
handsomX <javanav@163.com>
2021-03-05 10:40:44 +0800

    spring resource loader inject

---
+++ spring-study/039-spring-resourceloader-inject/src/main/java/constxiong/Test.java
@@ -0,0 +1,45 @@
+package constxiong;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.ResourceLoader;
+
+/**
+ * 测试 resource loader 注入
+ * 
+ * @author ConstXiong
+ */
+public class Test implements ResourceLoaderAware {
+	
+	private ResourceLoader resourceLoader;
+	
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
+	
+	@Autowired
+	private ResourceLoader injectedResourceLoader;
+	
+	@Autowired
+	private ApplicationContext context;
+	
+	@PostConstruct
+	public void print() {
+		//系统将 ApplicationContext 作为 Resource Loader
+		System.out.println(resourceLoader == injectedResourceLoader);
+		System.out.println(resourceLoader == context);
+	}
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
+		new AnnotationConfigApplicationContext(Test.class);
+	}
+
+}
