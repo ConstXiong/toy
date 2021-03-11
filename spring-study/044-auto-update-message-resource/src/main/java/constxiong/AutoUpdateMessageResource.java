f78789319ad78bc39e9fa1835060fbbf2f509c69
handsomX <javanav@163.com>
2021-03-11 11:14:24 +0800

    spring message resource auto update

--- spring-study/044-auto-update-message-resource/src/main/java/constxiong/AutoUpdateMessageResource.java
+++ spring-study/044-auto-update-message-resource/src/main/java/constxiong/AutoUpdateMessageResource.java
@@ -1,11 +1,25 @@
 package constxiong;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.ClosedWatchServiceException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.Watchable;
 import java.text.MessageFormat;
 import java.util.Locale;
+import java.util.Properties;
 
-import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.support.AbstractMessageSource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.StringUtils;
 
 /**
  * 监听文件自动更新 Properties
@@ -13,18 +27,107 @@ import org.springframework.core.io.ResourceLoader;
  * @author ConstXiong
  * @date 2021年3月8日 上午11:58:25
  */
-public class AutoUpdateMessageResource extends AbstractMessageSource implements ResourceLoaderAware {
-
+public class AutoUpdateMessageResource extends AbstractMessageSource {
 	
+	public static final String PROPERTIES_FILE_NAME = "msg.properties";
+
+	private Resource resource;
+	
+	private Properties properties;
+	
+	private boolean listen = true;
+	
+	private WatchService watchService;
+	
+	public AutoUpdateMessageResource() throws IOException {
+		ResourceLoader resourceLoader = new DefaultResourceLoader();
+		resource = resourceLoader.getResource(PROPERTIES_FILE_NAME);
+		properties = new Properties();
+		properties.load(resource.getInputStream());
+		listenProperteisChange();
+	}
+
+	/**
+	 * 监听properties文件修改
+	 */
+	private void listenProperteisChange() {
+		try {
+			Path filePath = this.resource.getFile().toPath();
+			Path dirPath = filePath.getParent();
+			this.watchService = FileSystems.getDefault().newWatchService();
+			dirPath.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+			dealFileChange(watchService);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 监听和处理文件修改
+	 */
+	private void dealFileChange(WatchService watchService) {
+		new Thread(() -> {
+			while (listen) {
+				WatchKey watchKey = null;
+				InputStream is = null;
+				try {
+					watchKey = watchService.take();
+					if (watchKey.isValid()) {
+						for (WatchEvent<?> event : watchKey.pollEvents()) {
+							Watchable watchable = watchKey.watchable();
+							Path dirPath = (Path)watchable;
+							Path relativePath = (Path)event.context();
+							if (PROPERTIES_FILE_NAME.equals(relativePath.getFileName().toString())) {
+								Path absolutePath = dirPath.resolve(relativePath);
+								Properties p = new Properties();
+								is = new FileInputStream(absolutePath.toFile());
+								p.load(is);
+								synchronized (this.properties) {
+									this.properties.clear();
+									this.properties.putAll(p);
+								}
+							}
+						}
+					}
+				} catch (ClosedWatchServiceException e)  {
+					this.listen = false;
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					if (watchKey != null) {
+						watchKey.reset();
+					}
+					if (is != null) {
+						try {
+							is.close();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}).start();
+	}
 
 	@Override
 	protected MessageFormat resolveCode(String code, Locale locale) {
+		String message = this.properties.getProperty(code);
+		if (!StringUtils.isEmpty(message)) {
+			return new MessageFormat(message, locale);
+		}
 		return null;
 	}
 
-	@Override
-	public void setResourceLoader(ResourceLoader resourceLoader) {
-		
+	public void stopListen(long delay) {
+		new Thread(() -> {
+			try {
+				Thread.sleep(delay);
+				this.listen = false;
+				this.watchService.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
 	}
-
+	
 }
--- spring-study/044-auto-update-message-resource/src/main/java/constxiong/Test.java
+++ spring-study/044-auto-update-message-resource/src/main/java/constxiong/Test.java
@@ -1,5 +1,8 @@
 package constxiong;
 
+import java.io.IOException;
+import java.util.Locale;
+
 
 /**
  * 测试 spring 自定义 MessageResource 自动更新监听的文件
@@ -8,10 +11,15 @@ package constxiong;
  */
 public class Test {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		AutoUpdateMessageResource mr = new AutoUpdateMessageResource();
-		
-		
+		//5 秒后停止监听
+		mr.stopListen(5 * 1000L);
+		//for 循环的时候手动修改 044-auto-update-message-resource\target\classes\msg.properties 文件 name 属性值，即可看到打印的值发生变化
+		for (int i = 0; i < 10; i++) {
+			System.out.println(mr.getMessage("name", null, Locale.getDefault()));
+			Thread.sleep(1 * 1000L);
+		}
 	}
 	
 }
